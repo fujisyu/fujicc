1430687289d15dcb286a87b53ae053a5de9d6bab
Hideaki Fujioka <h.f.lefty@gmail.com>
2018-11-10 16:02:06 +0900

    コメント位置の再調整

--- fujicc.c
+++ fujicc.c
@@ -11,16 +11,16 @@ enum {
 
 // トークンの型
 typedef struct {
-  int ty;							 // トークンの型
-  int val;             // tyがTK_NUMの場合、その数値
-  char *input;         // トークン文字列（エラーメッセージ用）
+  int ty;                      // トークンの型
+  int val;                     // tyがTK_NUMの場合、その数値
+  char *input;                 // トークン文字列（エラーメッセージ用）
 } Token;
 
 typedef struct Node {
-  int ty;							// 演算子かND_NUM
-  struct Node* lhs;		// 左辺
-  struct Node* rhs;   // 右辺
-  int val;            // tyがND_NUMの場合のみ使う
+  int ty;                       // 演算子かND_NUM
+  struct Node* lhs;             // 左辺
+  struct Node* rhs;             // 右辺
+  int val;                      // tyがND_NUMの場合のみ使う
 } Node;
 
 enum {
