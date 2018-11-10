5901c98c759d2bb3acac0c7f008ce38f953b9ff4
Hideaki Fujioka <h.f.lefty@gmail.com>
2018-11-10 15:51:39 +0900

    インデントからTABを無くした

--- fujicc.c
+++ fujicc.c
@@ -5,41 +5,41 @@
 
 // トークンの型を表す値
 enum {
-			TK_NUM = 256, // 整数トークン
-			TK_EOF,				// 入力の終わりを表すトークン
+      TK_NUM = 256,             // 整数トークン
+      TK_EOF,										// 入力の終わりを表すトークン
 };
 
 // トークンの型
 typedef struct {
-	int ty;            // トークンの型
-	int val;           // tyがTK_NUMの場合、その数値
-	char *input;       // トークン文字列（エラーメッセージ用）
+  int ty;							 // トークンの型
+  int val;										 // tyがTK_NUMの場合、その数値
+  char *input;								 // トークン文字列（エラーメッセージ用）
 } Token;
 
 typedef struct Node {
-	int ty;							// 演算子かND_NUM
-	struct Node* lhs;		// 左辺
-	struct Node* rhs;		// 右辺
-	int val;						// tyがND_NUMの場合のみ使う
+  int ty;							// 演算子かND_NUM
+  struct Node* lhs;		// 左辺
+  struct Node* rhs;							// 右辺
+  int val;											// tyがND_NUMの場合のみ使う
 } Node;
 
 enum {
-			ND_NUM = 256,		// 整数のノードの型
+      ND_NUM = 256,							// 整数のノードの型
 };
 
 Node* new_node (int op, Node *lhs, Node *rhs) {
-	Node *node = malloc(sizeof (Node));
-	node->ty = op;
-	node->lhs = lhs;
-	node->rhs = rhs;
-	return node;
+  Node *node = malloc(sizeof (Node));
+  node->ty = op;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
 }
 
 Node* new_node_num (int val) {
-	Node *node = malloc(sizeof (Node));
-	node->ty = ND_NUM;
-	node->val = val;
-	return node;
+  Node *node = malloc(sizeof (Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
 }
 
 int pos = 0;
@@ -47,42 +47,42 @@ Token tokens[100];
 
 // pが指している文字列をトークンに分割してtokensに保存する
 void tokenize (char *p) {
-	int i = 0;
-	while (*p) {
-		// 空白文字をスキップ
-		if (isspace(*p)) {
-			p++;
-			continue;
-		}
+  int i = 0;
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
 
-		if (*p == '+' || *p == '-' || *p == '/' || *p == '*' || *p == '(' || *p == ')' ) {
-			tokens[i].ty = *p;
-			tokens[i].input = p;
-			i++;
-			p++;
-			continue;
-		}
+    if (*p == '+' || *p == '-' || *p == '/' || *p == '*' || *p == '(' || *p == ')' ) {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
 
-		if (isdigit(*p)) {
-			tokens[i].ty = TK_NUM;
-			tokens[i].input = p;
-			tokens[i].val = strtol(p, &p, 10);
-			i++;
-			continue;
-		}
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
 
-		fprintf(stderr, "トークナイズできません: %s\n", p);
-		exit(1);
-	}
+    fprintf(stderr, "トークナイズできません: %s\n", p);
+    exit(1);
+  }
 
-	tokens[i].ty = TK_EOF;
-	tokens[i].input = p;
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
 }
 
 // エラーを報告するための関数
 void error(int i) {
-	fprintf(stderr,"予期せぬトークンです: %s\n", tokens[i].input);
-	exit(1);
+  fprintf(stderr,"予期せぬトークンです: %s\n", tokens[i].input);
+  exit(1);
 }
 
 Node *mul();
@@ -91,91 +91,91 @@ Node *term();
 
 
 Node *mul() {
-	Node *lhs = term();
-	if (tokens[pos].ty == TK_EOF) {
-		return lhs;
-	}
+  Node *lhs = term();
+  if (tokens[pos].ty == TK_EOF) {
+    return lhs;
+  }
 
-	if (tokens[pos].ty == '*') {
-		pos++;
-		return new_node('*', lhs, mul());
-	}
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
 
-	if (tokens[pos].ty == '/') {
-		pos++;
-		return new_node('/', lhs, mul());
-	}
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
 
-	return lhs;
+  return lhs;
 }
 
 Node *expr() {
-	Node *lhs = mul();
-	if (tokens[pos].ty == TK_EOF) {
-		return lhs;
-	}
+  Node *lhs = mul();
+  if (tokens[pos].ty == TK_EOF) {
+    return lhs;
+  }
 
-	if (tokens[pos].ty == '+') {
-		pos++;
-		return new_node('+', lhs, expr());
-	}
-	if (tokens[pos].ty == '-') {
-		pos++;
-		return new_node('-', lhs, expr());
-	}
-	return lhs;
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
 }
 
 Node *term() {
-	if (tokens[pos].ty == TK_NUM) {
-		return new_node_num(tokens[pos++].val);
-	}
+  if (tokens[pos].ty == TK_NUM) {
+    return new_node_num(tokens[pos++].val);
+  }
 
-	if (tokens[pos].ty == '(')
-		pos++;
-	Node *node = expr();
-	if (tokens[pos].ty != ')' )
-		error (pos);
+  if (tokens[pos].ty == '(')
+    pos++;
+  Node *node = expr();
+  if (tokens[pos].ty != ')' )
+    error (pos);
 
-	pos++;
-	return node;
+  pos++;
+  return node;
 
-	error(pos);
+  error(pos);
 
 }
 
 void gen (Node *node) {
-	if (node->ty == ND_NUM) {
-		printf("  push %d\n", node->val);
-		return;
-	}
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
 
-	gen(node->lhs);
-	gen(node->rhs);
+  gen(node->lhs);
+  gen(node->rhs);
 
-	printf("  pop rdi\n");
-	printf("  pop rax\n");
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
 
-	switch (node->ty) {
-	case '+':
-		printf("  add rax, rdi\n");
-		break;
-	case '-':
-		printf("  sub rax, rdi\n");
-		break;
-	case '*':
-		printf("  mul rdi\n");
-		break;
-	case '/':
-		printf("  mov rdx, 0\n");
-		printf("  div rdi\n");
-	}
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
 
-	printf(" push rax\n");
+  printf(" push rax\n");
 }
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
+  if (argc != 2) {
 		fprintf(stderr, "引数の個数が正しくありません\n");
 		return 1;
 	}
@@ -197,5 +197,5 @@ int main(int argc, char **argv) {
 	// それをRAXにロードして関数からの返り値とする
 	printf("  pop rax\n");
 	printf("  ret\n");
-	return 0;
+  return 0;
 }
