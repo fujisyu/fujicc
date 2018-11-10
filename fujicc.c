2a1190e7e5f7dc5ce9a1ac421071ca918725f076
Hideaki Fujioka <h.f.lefty@gmail.com>
2018-11-10 15:58:21 +0900

    コメント前の空白調整

--- fujicc.c
+++ fujicc.c
@@ -6,25 +6,25 @@
 // トークンの型を表す値
 enum {
       TK_NUM = 256,             // 整数トークン
-      TK_EOF,										// 入力の終わりを表すトークン
+      TK_EOF,                   // 入力の終わりを表すトークン
 };
 
 // トークンの型
 typedef struct {
   int ty;							 // トークンの型
-  int val;										 // tyがTK_NUMの場合、その数値
-  char *input;								 // トークン文字列（エラーメッセージ用）
+  int val;             // tyがTK_NUMの場合、その数値
+  char *input;         // トークン文字列（エラーメッセージ用）
 } Token;
 
 typedef struct Node {
   int ty;							// 演算子かND_NUM
   struct Node* lhs;		// 左辺
-  struct Node* rhs;							// 右辺
-  int val;											// tyがND_NUMの場合のみ使う
+  struct Node* rhs;   // 右辺
+  int val;            // tyがND_NUMの場合のみ使う
 } Node;
 
 enum {
-      ND_NUM = 256,							// 整数のノードの型
+      ND_NUM = 256,             // 整数のノードの型
 };
 
 Node* new_node (int op, Node *lhs, Node *rhs) {
